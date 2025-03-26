9c49eabdb942a7663557aa81273ae1fd25e3ffdf
ccosta25 <juliaortiz097@gmail.com>
2025-03-25 23:24:24 -0300

    cp 1

---
+++ src/main/java/br/com/fiap/checkpoint1/model/Pedido.java
@@ -0,0 +1,29 @@
+package br.com.fiap.checkpoint1.model;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do cliente é obrigatório")
+    private String clienteNome;
+
+    private LocalDate dataPedido = LocalDate.now();
+
+    @Min(value = 0, message = "O valor total do pedido não pode ser negativo")
+    private double valorTotal;
+}
